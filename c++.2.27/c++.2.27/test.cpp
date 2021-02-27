a4b7ae7cf6ff0433b0b6827cc4703098bc351104
jie-chen532 <3479676393@qq.com>
2021-02-27 23:44:44 +0800

    命名空间、函数重载

---
+++ c++.2.27/c++.2.27/test.cpp
@@ -0,0 +1,134 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+using namespace std;
+
+//namespace s
+//{
+//	int a = 10;
+//	void fun1()
+//	{
+//		cout << "this is a fun1" <<endl;
+//	}
+//
+//	namespace s1
+//	{
+//		int a = 10;
+//		int add(int a,int b)
+//		{
+//			return a + b;
+//		}
+//	}
+//}
+//
+//namespace s
+//{
+//	int b = 10;
+//	void fun2()
+//	{
+//		cout << "this is a fun2" << endl;
+//	}
+//}
+//
+//void test()
+//{
+//	using namespace s;
+//	cout << b << endl;
+//	fun2();
+//}
+
+//void test()
+//{
+//	using s::b;
+//	cout << b << endl;
+//	using s::fun2;
+//	fun2();
+//}
+
+//void test()
+//{
+//	cout << s::b << endl;
+//	s::fun2();
+//}
+
+//void fun(int a)
+//{
+//	cout << a << endl;
+//}
+
+//void fun1(int a = 10)
+//{
+//	cout << a << endl;
+//}
+//
+//void fun2(int a = 10, int b = 20, int c = 30)
+//{
+//	cout << a << "" << b << " " << c << endl;
+//}
+//
+//void test()
+//{
+//	fun1();
+//	fun1(100);
+//}
+
+//void fun(int a, int b = 10, int c = 20)
+//{
+//	cout << a << " " << b << " " << c << endl;
+//}
+
+//void fun1(int a = 10, int b, int c = 10)
+//{
+//	cout << a << " " << b << " " << c << endl;
+//}
+
+//void fun1(int a = 10);
+//
+//void fun1(int a = 10) 
+//{
+//	cout << a << endl;
+//}
+
+//void test()
+//{
+//	fun(10);
+//}
+
+//void fun(int a, int b)
+//{
+//	cout << a << " " << b << endl;
+//}
+//
+//void fun(double a, double b)
+//{
+//	cout << a << endl;
+//}
+//void fun(int a, int b, int c)
+//{
+//	cout << a << " " << b << " " << c << endl;
+//}
+
+//void fun(int a, double b)
+//{
+//	cout << a << endl;
+//}
+
+//void fun(int a, int b)
+//{
+//	cout << a << " " << b << endl;
+//}
+//
+//int fun(int a, int b)
+//{
+//	return a + b;
+//}
+
+//void fun(double a, int b)
+//{
+//	cout << a << endl;
+//}
+
+int main()
+{
+	//test();
+	return 0;
+}
