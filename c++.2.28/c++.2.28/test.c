eb954d7c61b945427a29cb2bd1be4becb4dca985
jie-chen532 <3479676393@qq.com>
2021-02-28 23:51:53 +0800

    练习

---
+++ c++.2.28/c++.2.28/test.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+//char addc(char a, char b);
+
+int add(int a, int b);
+char add(char a, char b);
+double add(double a, double b);
+
+
+int main()
+{
+	//int ret = add(2, 3);
+	//char retc = add('a', 'b');
+	//double retd = add(2.0, 3.0);
+	return 0;
+}
---
+++ c++.2.28/c++.2.28/test.cpp
@@ -0,0 +1,82 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<iostream>
+using namespace std;
+
+//int add(int a, int b)
+//{
+//	return a + b;
+//}
+//
+//double add(double a, double b)
+//{
+//	return a + b;
+//}
+
+//int add(int a, int b);
+//char add(char a, char b);
+//
+//int main()
+//{
+//	int ret = add(2, 3);
+//	char retc = add('a', 'b');
+//	return 0;
+//}
+
+//void test()
+//{
+//	int a = 10;
+//	int& ra = a;
+//
+//	cout << "a:" << a << endl;
+//	cout << "ra:" << ra << endl;
+//}
+
+//void test()
+//{
+//	int a = 10;
+//	int& ra = a;
+//	int& rb = a;
+//	int& rc = a;
+//}
+//
+//void test()
+//{
+//	int a = 10;
+//	int& ra = a;
+//}
+
+//void test()
+//{
+//	//int num = 7;
+//	//int sum = 7;
+//	//int a = (sum = num++, sum++, ++num);
+//	//cout << sum << endl;
+//
+//	int i = 10;
+//	int j = 10;
+//	int k = 3;
+//	k *= i + j;
+//	cout << k << endl; 
+//}
+
+
+//char * toLowerCase(char * str)
+//{
+//	char* ptr = str;
+//	while (*str != '\0')
+//	{
+//		if (*str >= 'A' && *str <= 'Z')
+//		{
+//			*str += 32;
+//		}
+//		str++;
+//	}
+//	return ptr;
+//}
+
+int main()
+{
+	//test();
+	return 0;
+}
