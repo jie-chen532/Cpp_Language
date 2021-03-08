670768675a0d28202144459d0ac1a544b6e3c696
jie-chen532 <3479676393@qq.com>
2021-03-08 22:32:00 +0800

    初始化列表

---
+++ c++.3.8/c++.3.8/test.cpp
@@ -0,0 +1,141 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<iostream>
+using namespace std;
+
+//class Date
+//{
+//public:
+//	Date(int year, int month, int day)
+//	{
+//		_year = year;
+//		_month = month;
+//		_day = day;
+//	}
+//
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+
+//class Date
+//{
+//public:
+//	Date(int year = 2021, int month = 3, int day = 10)
+//		:_year(2021)
+//		,_month(3)
+//		,_day(8)
+//	{}
+//
+//	void print()
+//	{
+//		cout << _year << "-" << _month << "-" << _day << endl;
+//	}
+//
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+
+
+//class Date
+//{
+//public:
+//	Date(int year, int month, int day)
+//		:_year(year)
+//		,_month(month)
+//		,_day(day)
+//	{}
+//
+//private:
+//	int& _year;
+//	int& _month;
+//	int& _day;
+//};
+
+
+//class Date
+//{
+//public:
+//	Date(int year, int month, int day)
+//		: _year(year)
+//		, _month(month)
+//		, _day(day)
+//	{}
+//
+//private:
+//	const int _year;
+//	const int _month;
+//	const int _day;
+//};
+
+//class Time
+//{
+//public:
+//	Time(int hour, int minute, int second)
+//		: _hour(hour)
+//		, _minute(minute)
+//		, _second(second)
+//	{}
+//
+//private:
+//	int _hour;
+//	int _minute;
+//	int _second;
+//};
+//
+//class Date
+//{
+//public:
+//	Date(int year, int month, int day)
+//		: _year(year)
+//		, _month(month)
+//		, _day(day)
+//		, _t(10, 20, 20)
+//	{}
+//
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//	Time _t;
+//};
+
+class Time
+{
+public:
+	Time(int hour = 0)
+		:_hour(hour)
+	{
+		cout << "Time()" << endl;
+	}
+private:
+	int _hour;
+};
+
+class Date
+{
+public:
+	Date(int day)
+	{}
+private:
+	int _day;
+	Time _t;
+};
+
+int main()
+{
+	Date d(1);
+}
+
+//void test()
+//{
+//	Date d(1);
+//}
+//
+//int main()
+//{
+//	test();
+//}
