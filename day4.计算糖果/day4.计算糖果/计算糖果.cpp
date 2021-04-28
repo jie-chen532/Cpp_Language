eab0b76d030631ebd6c05c3836093e45a28ee308
jie-chen532 <3479676393@qq.com>
2021-04-28 15:39:45 +0800

    计算糖果

---
+++ day4.计算糖果/day4.计算糖果/计算糖果.cpp
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c, d;
+	cin >> a >> b >> c >> d;
+	int A = (a + c) / 2;
+	int B1 = (c - a) / 2;
+	int B2 = (b + d) / 2;
+	int C = (d - b) / 2;
+	if (B1 != B2)
+		cout << "No" << endl;
+	else
+		cout << A << " " << B1 << " " << C << endl;
+	return 0;
+}
